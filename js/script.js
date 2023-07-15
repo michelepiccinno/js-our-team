f33c2bcb9be33203f0deb4f2e0f801128726b228
michelepiccinno <michele.piccinno@gmail.com>
2023-07-15 11:53:07 +0200

    add-imgs

--- js/script.js
+++ js/script.js
@@ -4,37 +4,37 @@ const arrayObjects = [
     firstName: "Wayne",
     lastName: "Barnett",
     role: "Founder & CEO",
-    img: "wayne-barnett-founder-ceo.jpg"
+    img: "/img/wayne-barnett-founder-ceo.jpg"
   },
   {
     firstName: "Angela",
     lastName: "Caroll",
     role: "Chief Editor",
-    img: "angela-caroll-chief-editor.jpg"
+    img: "/img/angela-caroll-chief-editor.jpg"
   },
   {
     firstName: "Walter",
     lastName: "Gordon",
     role: "Office Manage",
-    img: "walter-gordon-office-manager.jpg"
+    img: "/img/walter-gordon-office-manager.jpg"
   },
   {
     firstName: "Angela",
     lastName: "Lopez",
     role: "Social Media Manager",
-    img: "angela-lopez-social-media-manager.jpg"
+    img: "/img/angela-lopez-social-media-manager.jpg"
   },
   {
     firstName: "Scott",
     lastName: "Estrada",
     role: "Developer",
-    img: "scott-estrada-developer.jpg"
+    img: "/img/scott-estrada-developer.jpg"
   },
   {
     firstName: "Barbara",
     lastName: "Ramos",
     role: "Graphic Designer",
-    img: "barbara-ramos-graphic-designer.jpg"
+    img: "/img/barbara-ramos-graphic-designer.jpg"
   },
 ];
 
@@ -45,31 +45,36 @@ for (let i = 0; i < arrayObjects.length; i++) {
   //RICHIAMO IL CONTAINER PADRE 
   const carContainer = document.querySelector(".card-container");
 
-  //-CREO IL DIV, GLI ASSEGNO LA CLASSE "card" E LO APPENDO AL CONTAINER PADRE
+  //-CREO IL DIV, GLI ASSEGNO LA CLASSE "card" 
   const card = document.createElement("div");
   card.classList.add("card");
-  carContainer.appendChild(card);
 
   //RICHIAMO L'ELEMENTO APPENA CREATO CON CLASSE "card"
   const myCard = document.querySelector(".card");
 
-  //CREO UN H4, ASSEGNO CLASSE "first-name"  E LO APPENDO AL CONTAINER PADRE
+  //CREO UN IMG, ASSEGNO CLASSE image
+  const image = document.createElement("img");
+  image.classList.add("image");
+  image.src = arrayObjects[i].img;
+
+  //CREO UN H4, ASSEGNO CLASSE "first-name"  
   const firstName = document.createElement("h4");
   firstName.classList.add("first-name");
-  card.appendChild(firstName);
-  document.querySelector(".first-name").innerHTML = arrayObjects[i].firstName;
+  firstName.textContent = arrayObjects[i].firstName;
 
-  //CREO UN H4, ASSEGNO CLASSE "last-name"  E LO APPENDO AL CONTAINER PADRE
+  //CREO UN H4, ASSEGNO CLASSE "last-name" 
   const lastName = document.createElement("h4");
   lastName.classList.add("last-name");
-  card.appendChild(lastName);
+  lastName.textContent = arrayObjects[i].lastName;
 
-  //CREO UN IMG, ASSEGNO CLASSE image E LO APPENDO AL CONTAINER PADRE
-  const image = document.createElement("h4");
-  image.classList.add("image");
+
+
+  //APPENDO LA CARD E I SUOI ELEMENTI INTERNI
+  carContainer.appendChild(card);
   card.appendChild(image);
+  card.appendChild(firstName);
+  card.appendChild(lastName);
 
 }
 
 
-//document.querySelector(".first-name").innerHTML = "prova";
